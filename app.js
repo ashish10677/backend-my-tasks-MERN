205a6b71e189e38fc15fa65d857006f820566157
Ashish Kumar Mishra <ashish10677@gmail.com>
2018-06-08 20:49:12 +0530

    Add itemRouter.js itemRouter.js contains the routes to add, edit, update and delete the tasks.
    Signed-off-by: Ashish Kumar Mishra <ashish10677@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var port = 4200;
-app.listen(port, function() {
-    console.log("Hello World");
-})
+var cors = require('cors');
+
+// Mongoose connection with mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/mytasks', (err) => {
+    if(err) throw err;
+    console.log("Connected Successfuly");
+});
+
+// Required application specific custom router module
+var itemRouter = require('./src/routes/itemRouter');
+
+// Use middlewares to set view engine and post json data to the server
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/items', itemRouter);
+
+// Start the server
+app.listen(port, function(){
+  console.log('Server is running on Port: ',port);
+});
---
+++ src/routes/itemRouter.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var itemRouter = express.Router();
+
+var Item = require('../models/Item');
+
+//Route to add an element
+itemRouter.route('/add/post').post(function(req, res) {
+    console.log(req.body);
+    var item = new Item(req.body);
+    item.save()
+    .then(item => {
+        res.json('Item added successfully');
+    })
+    .catch(err => {
+        res.status(400).send('Unable to save to database');
+    });
+});
+
+//Route to fetch the data
+itemRouter.route('/').get((req, res) => {
+    Item.find(function(err, itms) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.json(itms); 
+        }
+    });
+});
+
+//Route to edit data
+itemRouter.route('/edit/:id').get((req, res) => {
+    var id = req.params.id;
+    Item.findById(id, function(err, item) {
+        res.json(item);
+    });
+});
+
+//Route to update data
+itemRouter.route('/update/:id').post((req, res) => {
+    Item.findById(req.params.id,function(err, item) {
+        if(!item) {
+            return next(new Error('Could not load Document'));
+        }
+        else {
+            item.item = req.body.item;
+            item.save().then(item => {
+                res.json('Update complete');
+            })
+            .catch(err => {
+                res.status(400).send("unable to update the database");
+            });
+        }
+    });
+});
+
+//Route to delete data
+itemRouter.route('/delete/:id').get((req, res) => {
+    Item.findByIdAndRemove({_id : req.params.id}, 
+        function(err, item) {
+            if(err) res.json(err);
+            else res.json('Successfully removed');
+        });
+});
+
+module.exports = itemRouter;
